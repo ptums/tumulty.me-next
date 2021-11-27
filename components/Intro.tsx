d6169dc4e562e6285b93dc57aa1749378cc0878f
Peter Tumulty <ptumulty923@gmail.com>
2021-11-27 09:43:03 -0500

    fixed up some capitalize issues

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,7 +6,7 @@ const Intro = () => (
     <Title>Peter F. Tumulty</Title>
     <SubTitle>Lead Web Developer</SubTitle>
     <Excerpt>
-      Hi there! My name is Peter and I&apos;m the Owner and Lead Developer at Tumulty Web Services.
+      Hi there! My name is Peter and I&apos;m the owner and lead developer at Tumulty Web Services.
       I am a web engineer with over ten years of experience and proud dog parent based out of Brick,
       NJ and my favorite thing to do is build software that makes an impact.
     </Excerpt>
