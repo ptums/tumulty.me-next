a4595d473174518764351584b43a691c606b369e
Peter Tumulty <ptumulty923@gmail.com>
2021-12-06 22:04:11 -0500

    added image optimization

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  webpack: function(config) {
+  image: {
+    minimumCacheTTL: 60 * 60 * 5,
+    deviceSizes: [640, 768, 1024, 1280, 1536],
+  },
+  webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
     })
     return config
-  }
+  },
 }
