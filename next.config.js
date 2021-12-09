50e2a4d91dcdcf1c83da4c205a2da93fdfbf2a41
Peter Tumulty <ptumulty923@gmail.com>
2021-12-09 10:43:06 -0500

    added log rocket and ga

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ module.exports = {
     minimumCacheTTL: 60 * 60 * 5,
     deviceSizes: [640, 768, 1024, 1280, 1536],
   },
+  env: {
+    NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
+  },
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
